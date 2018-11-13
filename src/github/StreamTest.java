30a32300c14dbbd61d486a60e7c6c851771bb6ad
尘逸 <shiqiang.zsq@alibaba-inc.com>
2018-11-13 17:17:07 +0800

    stream test

--- src/github/StreamTest.java
+++ src/github/StreamTest.java
@@ -17,42 +17,33 @@ import java.util.stream.Collectors;
 public class StreamTest {
 
     public static void main(String[] args) throws Exception {
-        int count = 10;
+        int count = 1000;
         int forCount = 1;
-        Test1(count,forCount);
-        Test2(count,forCount);
-        TestSum(count,forCount);
+        //Test1(count, forCount);
+        Test2(count, forCount);
+        TestSum(count, forCount);
     }
 
-    public static void Test1(int count,int forCount){
+    public static void Test1(int count, int forCount) {
         System.out.println("ListAddTest\n");
         List<BankAccountDTO> bankAccountDTOList = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             BankAccountDTO bankAccountDTO = new BankAccountDTO();
             bankAccountDTO.setAccountNo(i + "");
-            if (i % 4 == 0) {
-                bankAccountDTO.setAccountNo(null);
-            }
             bankAccountDTO.setBankName("name");
             bankAccountDTO.setBankCode("code");
             bankAccountDTOList.add(bankAccountDTO);
         }
         long forTimeCount = 0;
-
         long forEachTimeCount = 0;
-
         long streamTimeCount = 0;
-
         long parallelTimeCount = 0;
-
 
         for (int i = 0; i < forCount; i++) {
             Long fortimeStart = System.nanoTime();
             List<BankAccountDTO> bankAccountDTOfor = new ArrayList<>();
             for (BankAccountDTO bankAccountDTO : bankAccountDTOList) {
-                if (bankAccountDTO.getAccountNo() != null && !bankAccountDTO.getAccountNo().isEmpty()) {
-                    bankAccountDTOfor.add(bankAccountDTO);
-                }
+                bankAccountDTOfor.add(bankAccountDTO);
             }
             Long forTime = (System.nanoTime() - fortimeStart);
             //System.out.println("forTime:" + forTime);
@@ -60,10 +51,8 @@ public class StreamTest {
 
             Long forEachtimeStart = System.nanoTime();
             List<BankAccountDTO> bankAccountDTOforEach = new ArrayList<>();
-            bankAccountDTOList.forEach(bankAccountDTO ->{
-                if (bankAccountDTO.getAccountNo() != null && !bankAccountDTO.getAccountNo().isEmpty()) {
-                    bankAccountDTOforEach.add(bankAccountDTO);
-                }
+            bankAccountDTOList.forEach(bankAccountDTO -> {
+                bankAccountDTOforEach.add(bankAccountDTO);
             });
             Long forEachTime = System.nanoTime() - forEachtimeStart;
             //System.out.println("forEachTime:" + forEachTime);
@@ -71,7 +60,6 @@ public class StreamTest {
 
             Long streamTimeStart = System.nanoTime();
             List<BankAccountDTO> bankAccountDTOStream = bankAccountDTOList.stream()
-                .filter(account -> account.getAccountNo() != null && !account.getAccountNo().isEmpty())
                 .collect(Collectors.toList());
             Long streamTime = (System.nanoTime() - streamTimeStart);
             //System.out.println("streamTime:" + streamTime);
@@ -79,52 +67,45 @@ public class StreamTest {
 
             Long parallelStreamTimeStart = System.nanoTime();
             List<BankAccountDTO> bankAccountDTOparallelStream = bankAccountDTOList.parallelStream()
-                .filter(account -> account.getAccountNo() != null && !account.getAccountNo().isEmpty())
                 .collect(Collectors.toList());
             Long parallelStream = (System.nanoTime() - parallelStreamTimeStart);
             //System.out.println("parallelStream:" + parallelStream);
             parallelTimeCount += parallelStream;
 
-            //System.out.println("length:" + bankAccountDTOfor.size() + " " + bankAccountDTOStream.size()+ " " + bankAccountDTOparallelStream.size()+"\n");
+            //System.out.println("length:" + bankAccountDTOfor.size() + " " + bankAccountDTOStream.size()+ " " +
+            // bankAccountDTOparallelStream.size()+"\n");
         }
         System.out.println(
-            "CountNanos for:" + forTimeCount + " forLam:" + forEachTimeCount + " stream:" + streamTimeCount + " parrelStream:"
+            "CountNanos for:" + forTimeCount + " forLam:" + forEachTimeCount + " stream:" + streamTimeCount
+                + " parrelStream:"
                 + parallelTimeCount);
         System.out.println(
-            "CountMills for:" + forTimeCount / 1000000 + " forLam:" + forEachTimeCount / 1000000 + " stream:" + streamTimeCount / 1000000 + " parrelStream:"
-                + parallelTimeCount / 1000000+"\n");
+            "CountMills for:" + forTimeCount * 1.0f / 1000000 + " forLam:" + forEachTimeCount * 1.0f / 1000000
+                + " stream:" + streamTimeCount * 1.0f / 1000000 + " parrelStream:"
+                + parallelTimeCount * 1.0f / 1000000 + "\n");
     }
 
-
-    public static void Test2(int count,int forCount){
+    public static void Test2(int count, int forCount) {
         System.out.println("MapAddTest");
         List<BankAccountDTO> bankAccountDTOList = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             BankAccountDTO bankAccountDTO = new BankAccountDTO();
             bankAccountDTO.setAccountNo(i + "");
-            if (i % 4 == 0) {
-                bankAccountDTO.setAccountNo(null);
-            }
             bankAccountDTO.setBankName("name");
             bankAccountDTO.setBankCode("code");
             bankAccountDTOList.add(bankAccountDTO);
         }
         long forTimeCount = 0;
-
         long forEachTimeCount = 0;
-
         long streamTimeCount = 0;
-
         long parallelTimeCount = 0;
-
-
 
         for (int i = 0; i < forCount; i++) {
             Long fortimeStart = System.nanoTime();
             Map<String, BankAccountDTO> bankAccountDTOfor = new HashMap<>();
             for (BankAccountDTO bankAccountDTO : bankAccountDTOList) {
                 if (bankAccountDTO.getAccountNo() != null && !bankAccountDTO.getAccountNo().isEmpty()) {
-                    bankAccountDTOfor.put(bankAccountDTO.getAccountNo(),bankAccountDTO);
+                    bankAccountDTOfor.put(bankAccountDTO.getAccountNo(), bankAccountDTO);
                 }
             }
             Long forTime = (System.nanoTime() - fortimeStart);
@@ -133,10 +114,8 @@ public class StreamTest {
 
             Long forEachtimeStart = System.nanoTime();
             Map<String, BankAccountDTO> bankAccountDTOforEach = new HashMap<>();
-            bankAccountDTOList.forEach(bankAccountDTO ->{
-                if (bankAccountDTO.getAccountNo() != null && !bankAccountDTO.getAccountNo().isEmpty()) {
-                    bankAccountDTOforEach.put(bankAccountDTO.getAccountNo(),bankAccountDTO);
-                }
+            bankAccountDTOList.forEach(bankAccountDTO -> {
+                bankAccountDTOforEach.put(bankAccountDTO.getAccountNo(), bankAccountDTO);
             });
             Long forEachTime = System.nanoTime() - forEachtimeStart;
             //System.out.println("forEachTime:" + forEachTime);
@@ -144,7 +123,6 @@ public class StreamTest {
 
             Long streamTimeStart = System.nanoTime();
             Map<String, BankAccountDTO> bankAccountDTOStream = bankAccountDTOList.stream()
-                .filter(account -> account.getAccountNo() != null && !account.getAccountNo().isEmpty())
                 .collect(Collectors.toMap(BankAccountDTO::getAccountNo, Function.identity()));
             Long streamTime = (System.nanoTime() - streamTimeStart);
             //System.out.println("streamTime:" + streamTime);
@@ -152,7 +130,6 @@ public class StreamTest {
 
             Long parallelStreamTimeStart = System.nanoTime();
             Map<String, BankAccountDTO> bankAccountDTOparallelStream = bankAccountDTOList.parallelStream()
-                .filter(account -> account.getAccountNo() != null && !account.getAccountNo().isEmpty())
                 .collect(Collectors.toMap(BankAccountDTO::getAccountNo, Function.identity()));
             Long parallelStream = (System.nanoTime() - parallelStreamTimeStart);
             //System.out.println("parallelStream:" + parallelStream);
@@ -163,16 +140,18 @@ public class StreamTest {
         }
 
         System.out.println(
-            "CountNonos forEach:" + forTimeCount + " forLam:" + forEachTimeCount + " stream:" + streamTimeCount + " parrelStream:"
+            "CountNonos forEach:" + forTimeCount + " forLam:" + forEachTimeCount + " stream:" + streamTimeCount
+                + " parrelStream:"
                 + parallelTimeCount);
         System.out.println(
-            "CountMills forEach:" + forTimeCount / 1000000 + " forLam:" + forEachTimeCount / 1000000 + " stream:" + streamTimeCount / 1000000 + " parrelStream:"
-                + parallelTimeCount / 1000000+"\n");
+            "CountMills forEach:" + forTimeCount * 1.0f / 1000000 + " forLam:" + forEachTimeCount * 1.0f / 1000000
+                + " stream:" + streamTimeCount * 1.0f / 1000000 + " parrelStream:"
+                + parallelTimeCount * 1.0f / 1000000 + "\n");
 
     }
 
-    public static void TestSum(int count,int forCount) {
-        System.out.println("ListSumTest\n");
+    public static void TestSum(int count, int forCount) {
+        System.out.println("ListSumTest");
         List<BankAccountDTO> bankAccountDTOList = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             BankAccountDTO bankAccountDTO = new BankAccountDTO();
@@ -190,7 +169,6 @@ public class StreamTest {
         long streamTimeCount = 0;
         long parallelTimeCount = 0;
 
-
         for (int i = 0; i < forCount; i++) {
             Long fortimeStart = System.nanoTime();
             Long forSum = 0L;
@@ -206,7 +184,7 @@ public class StreamTest {
 
             Long forEachtimeStart = System.nanoTime();
             Wrapper forEachSum = new Wrapper();
-            bankAccountDTOList.forEach(bankAccountDTO ->{
+            bankAccountDTOList.forEach(bankAccountDTO -> {
                 if (bankAccountDTO.getAccountNo() != null && !bankAccountDTO.getAccountNo().isEmpty()) {
                     forEachSum.setValue(forEachSum.getValue() + Long.valueOf(bankAccountDTO.getAccountNo()));
                 }
@@ -217,7 +195,7 @@ public class StreamTest {
 
             Long forEachLamtimeStart = System.nanoTime();
             Wrapper forEachLamSum = new Wrapper();
-            bankAccountDTOList.forEach(bankAccountDTO ->{
+            bankAccountDTOList.forEach(bankAccountDTO -> {
                 if (bankAccountDTO.getAccountNo() != null && !bankAccountDTO.getAccountNo().isEmpty()) {
                     forEachLamSum.setValue(forEachLamSum.getValue() + Long.valueOf(bankAccountDTO.getAccountNo()));
                 }
@@ -244,17 +222,21 @@ public class StreamTest {
             //System.out.println("parallelStream:" + parallelStream);
             parallelTimeCount += parallelStream;
 
-            //System.out.println("forSum:"+forSum+" forEachSum:"+forEachSum.getValue()+" streamSum:"+streamSum+" parallelSum:"+parallelStreamSum);
-            //System.out.println("length:" + bankAccountDTOfor.size() + " " + bankAccountDTOStream.size()+ " " + bankAccountDTOparallelStream.size()+"\n");
+            //System.out.println("forSum:"+forSum+" forEachSum:"+forEachSum.getValue()+" streamSum:"+streamSum+"
+            // parallelSum:"+parallelStreamSum);
+            //System.out.println("length:" + bankAccountDTOfor.size() + " " + bankAccountDTOStream.size()+ " " +
+            // bankAccountDTOparallelStream.size()+"\n");
         }
-        System.out.println();
         System.out.println(
-            "CountNanos for:" + forTimeCount + " forEach:" + forEachTimeCount +" forLam:"+forEachLamTimeCount +" stream:" + streamTimeCount + " parrelStream:"
+            "CountNanos for:" + forTimeCount + " forEach:" + forEachTimeCount + " forLam:" + forEachLamTimeCount
+                + " stream:" + streamTimeCount + " parrelStream:"
                 + parallelTimeCount);
         System.out.println(
-            "CountMills for:" + forTimeCount / 1000000 + " forEach:" + forEachTimeCount / 1000000 + " forLam:"
-                + forEachLamTimeCount / 1000000 + " stream:" + streamTimeCount / 1000000 + " parrelStream:"
-                + parallelTimeCount / 1000000 + "\n");
+            "CountMills for:" + forTimeCount * 1.0f / 1000000 + " forEach:" + forEachTimeCount * 1.0f / 1000000
+                + " forLam:"
+                + forEachLamTimeCount * 1.0f / 1000000 + " stream:" + streamTimeCount * 1.0f / 1000000
+                + " parrelStream:"
+                + parallelTimeCount * 1.0f / 1000000 + "\n");
 
     }
 }
